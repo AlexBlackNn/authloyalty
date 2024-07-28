ebf42a7b2d552e44ba140e6984c0c1e986b0c942
alex <alexblacknn@yandex.ru>
2024-07-28 18:16:09 +0300

    move logger to folder logger

--- cmd/sso/main.go
+++ cmd/sso/main.go
@@ -3,58 +3,16 @@ package main
 import (
 	"github.com/AlexBlackNn/authloyalty/app"
 	"github.com/AlexBlackNn/authloyalty/internal/config"
+	"github.com/AlexBlackNn/authloyalty/internal/logger"
 	"log/slog"
-	"os"
 )
 
 func main() {
 	// init config
 	cfg := config.MustLoad()
 	// init logger
-	log := setupLogger(cfg.Env)
+	log := logger.New(cfg.Env)
 	log.Info("starting application", slog.String("env", cfg.Env))
 	// init app
 	app.New(log, cfg)
-}
-
-// our environments
-const (
-	envLocal = "local"
-	envDemo  = "demo"
-	envProd  = "prod"
-)
-
-func setupLogger(env string) *slog.Logger {
-	var log *slog.Logger
-
-	switch env {
-	case envLocal:
-		log = slog.New(
-			slog.NewTextHandler(
-				os.Stdout, &slog.HandlerOptions{
-					Level:     slog.LevelDebug,
-					AddSource: true,
-				},
-			),
-		)
-	case envDemo:
-		log = slog.New(
-			slog.NewJSONHandler(
-				os.Stdout, &slog.HandlerOptions{
-					Level:     slog.LevelDebug,
-					AddSource: true,
-				},
-			),
-		)
-	case envProd:
-		log = slog.New(
-			slog.NewJSONHandler(
-				os.Stdout, &slog.HandlerOptions{
-					Level:     slog.LevelInfo,
-					AddSource: true,
-				},
-			),
-		)
-	}
-	return log
 }
