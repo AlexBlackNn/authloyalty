152f92312542a69793d732fb135338318e4cfbee
alex <alexblacknn@yandex.ru>
2024-07-29 20:36:08 +0300

    add test header to kafka producer

--- pkg/broker/producer.go
+++ pkg/broker/producer.go
@@ -34,12 +34,28 @@ func NewProducer(kafkaURL, srURL string) (*Producer, error) {
 		for e := range p.Events() {
 			switch ev := e.(type) {
 			case *kafka.Message:
-				if ev.TopicPartition.Error != nil {
-					//TODO: need some how to inform that the message was not send, m.b. write in database?
-					fmt.Printf("Delivery failed: %v\n", ev.TopicPartition)
+				// The message delivery report, indicating success or
+				// permanent failure after retries have been exhausted.
+				// Application level retries won't help since the client
+				// is already configured to do that.
+				m := ev
+				if m.TopicPartition.Error != nil {
+					fmt.Printf("Delivery failed: %v\n", m.TopicPartition.Error)
 				} else {
-					fmt.Printf("Delivered message to %v\n", ev.TopicPartition)
+					fmt.Printf("Delivered message to topic %s [%d] at offset %v\n",
+						*m.TopicPartition.Topic, m.TopicPartition.Partition, m.TopicPartition.Offset)
 				}
+			case kafka.Error:
+				// Generic client instance-level errors, such as
+				// broker connection failures, authentication issues, etc.
+				//
+				// These errors should generally be considered informational
+				// as the underlying client will automatically try to
+				// recover from any errors encountered, the application
+				// does not need to take action on them.
+				fmt.Printf("Error: %v\n", ev)
+			default:
+				fmt.Printf("Ignored event: %s\n", ev)
 			}
 		}
 	}()
@@ -65,6 +81,7 @@ func (p *Producer) Send(msg proto.Message, topic string) error {
 	if err = p.producer.Produce(&kafka.Message{
 		TopicPartition: kafka.TopicPartition{Topic: &topic},
 		Value:          payload,
+		Headers:        []kafka.Header{{Key: "request-Id", Value: []byte("header values are binary")}},
 	}, nil); err != nil {
 		return err
 	}
