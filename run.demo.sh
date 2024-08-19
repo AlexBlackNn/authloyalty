f69cc989a7174e5d4f5f29436f7988f3022d3216
alex <alexblacknn@yandex.ru>
2024-08-19 21:29:25 +0300

    delete unused files

--- run.demo.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-docker rm -f $(docker ps -aq)
-cd infra && docker-compose -f docker-compose.demo.yaml up -d
-sleep 15
-cd ..
-go run ./cmd/migrator/postgres  --migrations-path=./migrations
-
