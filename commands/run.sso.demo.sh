9fb51bb068d86293d989c0a9a7f26853207120c0
alex <alexblacknn@yandex.ru>
2024-08-19 21:28:46 +0300

    add how to launch demo

---
+++ commands/run.sso.demo.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+docker rm -f $(docker ps -aq)
+cd ../infra && docker compose -f docker-compose.prod.yaml up -d
+timeout 90s bash -c "until docker exec patroni1 pg_isready ; do sleep 5 ; done"
+cd .. && go run ./cmd/migrator/postgres  --p ./migrations -d postgres://postgres:postgres@localhost:5000/postgres?sslmode=disable
