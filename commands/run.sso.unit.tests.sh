0cacc8104ac332ebf08a9cd29cb6ec5bb356f289
alex <alexblacknn@yandex.ru>
2024-08-21 16:03:47 +0300

    add unit tests for registration, all infra is mocked

--- commands/run.sso.unit.tests.sh
+++ commands/run.sso.unit.tests.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-docker rm -f $(docker ps -aq)
-cd ../infra && docker compose -f docker-compose.local.yaml up -d
-timeout 90s bash -c "until docker exec patroni1 pg_isready ; do sleep 5 ; done"
-cd .. && go run ./cmd/migrator/postgres  --p ./tests/integragtion_tests/migrations -d postgres://postgres:postgres@localhost:5000/postgres?sslmode=disable
-pwd
-cd ./tests/unit_tests && go test *.go -v
+cd ../tests/unit_tests && go test *.go -v
