177b7e31faad845df45a27224496964c6e40fc47
alex <alexblacknn@yandex.ru>
2024-08-26 17:32:01 +0300

    create tables for loyalty service

---
+++ loyalty/internal/handlershttp/http/v1/bad_request_handlers.go
@@ -0,0 +1,60 @@
+package v1
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+
+	"github.com/AlexBlackNn/authloyalty/loyalty/internal/domain"
+	"github.com/AlexBlackNn/authloyalty/loyalty/internal/dto"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-playground/validator/v10"
+	"github.com/google/uuid"
+	jsoniter "github.com/json-iterator/go"
+)
+
+func handleAddLoyaltyBadRequest(w http.ResponseWriter, r *http.Request, reqData *dto.UserLoyalty) (*dto.UserLoyalty, error) {
+	if r.Method != http.MethodPost {
+		dto.ResponseErrorNowAllowed(w, "only POST method allowed")
+		return nil, errors.New("method not allowed")
+	}
+
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		dto.ResponseErrorBadRequest(w, "failed to read body")
+		return nil, errors.New("failed to read body")
+	}
+	var json = jsoniter.ConfigCompatibleWithStandardLibrary
+	err = json.Unmarshal(body, reqData)
+	if err != nil {
+		fmt.Println(reqData)
+		dto.ResponseErrorBadRequest(w, "failed to decode body")
+		return nil, errors.New("failed to decode request")
+	}
+
+	if err = validator.New().Struct(reqData); err != nil {
+		var validateErr validator.ValidationErrors
+		if errors.As(err, &validateErr) {
+			dto.ResponseErrorBadRequest(w, dto.ValidationError(validateErr))
+			return nil, errors.New("validation error")
+		}
+		dto.ResponseErrorBadRequest(w, "bad request")
+		return nil, errors.New("bad request")
+	}
+	return reqData, nil
+}
+
+func handleGetLoyaltyBadRequest(w http.ResponseWriter, r *http.Request) (*domain.UserLoyalty, error) {
+	if r.Method != http.MethodGet {
+		dto.ResponseErrorNowAllowed(w, "only Get method allowed")
+		return nil, errors.New("method not allowed")
+	}
+	currentUUID := chi.URLParam(r, "uuid")
+	_, err := uuid.Parse(currentUUID)
+	if err != nil {
+		dto.ResponseErrorNowAllowed(w, "invalid uuid")
+		return nil, errors.New("invalid uuid")
+	}
+	return &domain.UserLoyalty{UUID: currentUUID}, nil
+}
--- loyalty/internal/services/loyaltyservice/loyalty.go
+++ loyalty/internal/services/loyaltyservice/loyalty.go
@@ -2,22 +2,26 @@ package loyaltyservice
 
 import (
 	"context"
+	"fmt"
 	"log/slog"
 
 	"github.com/AlexBlackNn/authloyalty/loyalty/internal/config"
 	"github.com/AlexBlackNn/authloyalty/loyalty/internal/domain"
 	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/codes"
+	"go.opentelemetry.io/otel/trace"
 )
 
 type loyaltyStorage interface {
 	AddLoyalty(
 		ctx context.Context,
-		loyalty domain.UserLoyalty,
-	) (context.Context, domain.UserLoyalty, error)
+		loyalty *domain.UserLoyalty,
+	) (context.Context, *domain.UserLoyalty, error)
 	GetLoyalty(
 		ctx context.Context,
-		loyalty domain.UserLoyalty,
-	) (context.Context, domain.UserLoyalty, error)
+		loyalty *domain.UserLoyalty,
+	) (context.Context, *domain.UserLoyalty, error)
 	HealthCheck(context.Context) (context.Context, error)
 	Stop() error
 }
@@ -41,7 +45,7 @@ func New(
 	}
 }
 
-var tracer = otel.Tracer("sso service")
+var tracer = otel.Tracer("loyalty service")
 
 // HealthCheck returns service health check.
 func (l *Loyalty) HealthCheck(ctx context.Context) (context.Context, error) {
@@ -56,15 +60,69 @@ func (l *Loyalty) HealthCheck(ctx context.Context) (context.Context, error) {
 func (l *Loyalty) GetLoyalty(
 	ctx context.Context,
 	userLoyalty *domain.UserLoyalty,
-) (domain.UserLoyalty, error) {
-	return domain.UserLoyalty{UUID: "e1d07926-0dda-4b1c-a284-1919da8da752", Value: 100}, nil
+) (context.Context, *domain.UserLoyalty, error) {
+	const op = "SERVICE LAYER: auth_service.RegisterNewUser"
+
+	ctx, span := tracer.Start(ctx, "service layer: GetLoyalty",
+		trace.WithAttributes(attribute.String("handler", "GetLoyalty")))
+	defer span.End()
+
+	log := l.log.With(
+		slog.String("trace-id", "trace-id"),
+		slog.String("user-id", "user-id"),
+	)
+	log.Info("getting loyalty for user")
+
+	ctx, userLoyalty, err := l.loyalStorage.GetLoyalty(ctx, userLoyalty)
+	if err != nil {
+		span.SetStatus(codes.Error, err.Error())
+		span.SetAttributes(attribute.Bool("error", true))
+		span.RecordError(fmt.Errorf("%s: failed to get loyalty: %w", op, err))
+		log.Error("failed to get loyalty", "err", err.Error())
+		return ctx, nil, fmt.Errorf("%s: %w", op, err)
+	}
+	span.AddEvent(
+		"user loyalty extracted",
+		trace.WithAttributes(
+			attribute.String("user-id", userLoyalty.UUID),
+			attribute.Int("user-id", userLoyalty.Value),
+		))
+
+	return ctx, userLoyalty, nil
 }
 
 func (l *Loyalty) AddLoyalty(
 	ctx context.Context,
 	userLoyalty *domain.UserLoyalty,
 ) (domain.UserLoyalty, error) {
-	return domain.UserLoyalty{UUID: "e1d07926-0dda-4b1c-a284-1919da8da752", Value: 100}, nil
+	const op = "SERVICE LAYER: auth_service.RegisterNewUser"
+
+	ctx, span := tracer.Start(ctx, "service layer: AddLoyalty",
+		trace.WithAttributes(attribute.String("handler", "AddLoyalty")))
+	defer span.End()
+
+	log := l.log.With(
+		slog.String("trace-id", "trace-id"),
+		slog.String("user-id", "user-id"),
+	)
+	log.Info("getting loyalty for user")
+
+	ctx, userLoyalty, err := l.loyalStorage.GetLoyalty(ctx, userLoyalty)
+	if err != nil {
+		span.SetStatus(codes.Error, err.Error())
+		span.SetAttributes(attribute.Bool("error", true))
+		span.RecordError(fmt.Errorf("%s: failed to get loyalty: %w", op, err))
+		log.Error("failed to get loyalty", "err", err.Error())
+		return ctx, nil, fmt.Errorf("%s: %w", op, err)
+	}
+	span.AddEvent(
+		"user loyalty extracted",
+		trace.WithAttributes(
+			attribute.String("user-id", userLoyalty.UUID),
+			attribute.Int("user-id", userLoyalty.Value),
+		))
+
+	return ctx, userLoyalty, nil
 }
 
 func (l *Loyalty) SubLoyalty(
