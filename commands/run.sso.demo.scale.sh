a5d3d6edc98e5be4338198453ba182a35c877bdd
alex <alexblacknn@yandex.ru>
2024-08-31 14:04:21 +0300

    connect to sso via grpc balancer

--- commands/run.sso.demo.scale.sh
+++ commands/run.sso.demo.scale.sh
@@ -2,5 +2,5 @@
 docker rm -f $(docker ps -aq)
 cd ../infra && docker compose -f docker-compose.prod.scale.yaml up -d
 timeout 90s bash -c "until docker exec patroni1 pg_isready ; do sleep 5 ; done"
-cd .. && go run ./sso/cmd/migrator/postgres  --p ./sso/migrations -d postgres://postgres:postgres@localhost:5000/postgres?sslmode=disable
-cd ./infra && docker compose -f docker-compose.prod.scale.yaml scale sso=2
+cd ../commands && go run ./migrator/postgres  --p ./migrations -d postgres://postgres:postgres@localhost:5000/postgres?sslmode=disable
+cd ../infra && docker compose -f docker-compose.prod.scale.yaml scale sso=2
